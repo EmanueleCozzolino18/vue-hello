91d73c1fff0df17046432ef2e7a05488941cdff4
EmanueleCozzolino18 <emanuele.cozzolino18@libero.it>
2024-05-20 14:45:33 +0200

    [BONUS] Immagine inserita tramite data

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            benvenuto: 'Benvenuto Vue'
+            benvenuto: 'Benvenuto Vue',
+            immagine: 'https://m.media-amazon.com/images/I/816xM732cvL.jpg'
         }
     }
 }).mount('#app')
